6eb0608d2498d12d68f38228242890789de6afd8
nixiesquid <audu817@gmail.com>
2020-04-30 03:30:19 +0900

    implement step2

--- 9cc/9cc.c
+++ 9cc/9cc.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -7,10 +8,25 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  char *p = argv[1];
+
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
-  printf("  mov rax, %d\n", atoi(argv[1]));
+  printf("  mov rax, %ld\n", strtol(p, &p, 10));
+
+  while (*p) {
+    if (*p == '+' || *p == '-') {
+      bool isAdd = (*p == '+');
+      p++;
+      printf("  %s rax, %ld\n", isAdd ? "add" : "sub", strtol(p, &p, 10));
+      continue;
+    }
+
+    fprintf(stderr, "unexoected chars: '%c'\n", *p);
+    return 1;
+  }
+
   printf("  ret\n");
   return 0;
 }
